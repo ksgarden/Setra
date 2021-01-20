d6625427844b31ea4cb12b0b2f3f8a5593a4f02d
Karen Jardim Simões <karenjardim2001@gmail.com>
2021-01-20 10:50:58 -0400

    Add files via upload

---
+++ Setra/estilo/sidebar.js
@@ -0,0 +1,10 @@
+ 
+function mostrar() {
+    var y = document.getElementById('usuario')
+    if(y.style.display == 'none'){
+        y.style.display = 'block'
+    }else{
+        y.style.display = 'none'
+
+}
+}
